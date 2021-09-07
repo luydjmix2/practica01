8e8d344c71f6a90fd038b38081d50cc96db5629a
davidsant13 <practicante01@nes360.org>
2021-09-07 09:52:12 -0500

    new

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -23,6 +23,8 @@
     <body class="antialiased">
 
     <h2>Laravel practica</h2>
+    <p>esto es solo un pequeño comentario de actualización</p>
+
         <div class="relative flex items-top justify-center min-h-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
             @if (Route::has('login'))
                 <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
